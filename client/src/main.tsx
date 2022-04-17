2e6fc46ce6c94ca313b006a72bebd966ddaf650d
Freddy Cigollen <20170887@ce.pucmm.edu.do>
2022-04-17 01:43:42 -0400

    Set client structure

---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import "./Assets/Styles/normalize.css";
+import "./Assets/Styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
