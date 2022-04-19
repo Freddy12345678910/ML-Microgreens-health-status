0b2d17d66c4611100187100ff20a06c19edadba4
Freddy Cigollen <20170887@ce.pucmm.edu.do>
2022-04-19 00:24:31 -0400

    Change header to 'AppBar'

--- client/src/Layouts/Header/Header.tsx
+++ client/src/Layouts/Header/Header.tsx
@@ -1,13 +1,25 @@
-import logo from '../../Assets/Images/logo.svg';
+import { AppBar } from "@mui/material";
+import Logo from "../../Assets/Images/logo.svg";
 
 import "../../Assets/Styles/header.css";
 
 function Header() {
   return (
-  <header className="header">
-    <img className="header__logo" src={logo} />
-  </header>
+    <AppBar sx={styles.container}>
+      <img src={Logo} style={styles.logo} />
+    </AppBar>
   );
 }
+
+const styles = {
+  container: {
+    padding: "0.4em 0.4em 0.4em 0.8em",
+    background: "white",
+    boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.2)",
+  },
+  logo: {
+    width: "250px",
+  },
+};
 
 export default Header;
