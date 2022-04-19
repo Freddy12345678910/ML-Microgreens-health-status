b268051433324ba7571286a7ca5c1a63288c2af5
Freddy Cigollen <20170887@ce.pucmm.edu.do>
2022-04-18 22:42:40 -0400

    Set mobile nav item

--- client/src/Components/MobileNav/MobileNav.tsx
+++ client/src/Components/MobileNav/MobileNav.tsx
@@ -1,4 +1,6 @@
-import { Paper, BottomNavigation, Box } from "@mui/material";
+import { Paper, BottomNavigation } from "@mui/material";
+
+import MobileNavItem from "./MobileNavItem";
 
 import HomeIcon from "../../Assets/Images/home.png";
 import RecommendationsIcon from "../../Assets/Images/recommendations.png";
@@ -7,15 +9,8 @@ function MobileNav() {
   return (
     <Paper sx={styles.navContainer} elevation={2}>
       <BottomNavigation sx={styles.nav}>
-        <Box sx={styles.navItem.container}>
-          <img src={HomeIcon} style={styles.navItem.icon} />
-          <Box sx={styles.navItem.bottomBar} />
-        </Box>
-
-        <Box sx={styles.navItem.container}>
-          <img src={RecommendationsIcon} style={styles.navItem.icon} />
-          {/*<Box sx={styles.navItem.bottomBar} />*/}
-        </Box>
+        <MobileNavItem to={"/"} iconSrc={HomeIcon} />
+        <MobileNavItem to={"/recommendations"} iconSrc={RecommendationsIcon} />
       </BottomNavigation>
     </Paper>
   );
@@ -28,22 +23,6 @@ const styles = {
     alignItems: "center",
     justifyContent: "space-around",
     backgroundColor: "#3EC461",
-  },
-  navItem: {
-    container: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      width: "50%",
-      ":hover": { cursor: "pointer" },
-    },
-    icon: { margin: ".2em auto", width: "40px" },
-    bottomBar: {
-      height: "4px",
-      width: "50%",
-      background: "white",
-    },
   },
 };
 
---
+++ client/src/types/types.d.ts
@@ -0,0 +1,6 @@
+declare namespace Nav {
+  type NavItemProps = {
+    to: string;
+    iconSrc: string;
+  };
+}
